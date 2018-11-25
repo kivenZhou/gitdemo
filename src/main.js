ecfaa76293f65d761c8c459fee854a4254354e8a
1120576117@qq.com <zhouzhongyu@innotechx.com>
2018-11-25 20:48:30 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+Vue.config.productionTip = false
+
+Vue.prototype.$message = function(name) {
+  alert(name)
+}
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
